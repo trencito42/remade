195906a04f4439b1247ad2ba75c3c446499fa4e3
Stefan <alex@xodo.ro>
2026-09-18 05:03:31 +0300

    Unify project workflow shell

--- src/app/projects/[id]/directions/page.tsx
+++ src/app/projects/[id]/directions/page.tsx
@@ -6,7 +6,7 @@ type Props = { params: Promise<{ id: string }> };
 export default async function DirectionsPage({ params }: Props) {
   const { id } = await params;
   return (
-    <main className="shell wide">
+    <main className="shell wide workflow-shell">
       <header className="topbar">
         <Link className="brand" href="/">
           Remade
